93e4e328b565aa715859f123ac9218cfb7ebe05d
Carlos Eduardo Santos Oliveira <carlosoliveira@cpejr.com.br>
2022-10-26 11:50:32 -0300

    Criando página do usuário

---
+++ projetocpegp1/src/Pages/Usuário/Usuário.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {useState} from 'react';
+
+function App() {
+  
+  const [nome, setNome] =  useState();
+
+  return (
+    <div>
+    <button>EDITAR PERFIL</button>
+    </div>
+  );
+}
+
+export default App;
