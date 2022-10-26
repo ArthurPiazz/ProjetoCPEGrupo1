12b23b748cbef8e9fe960ef4173ce6a5fcb9b3df
Carlos Eduardo Santos Oliveira <carlosoliveira@cpejr.com.br>
2022-10-26 15:13:47 -0300

    Rotas

--- projetocpegp1/src/routes.js
+++ projetocpegp1/src/routes.js
@@ -4,16 +4,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Cadastro from "./Pages/Cadastro";
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
-import Usuario from "./Pages/Usuário";
+import Usuario from "./Pages/Usuario";
 
     function Rotas(){
         return(
             <BrowserRouter>
             <Routes>
-                <Route path="/cadastro" component ={Cadastro} />
-                <Route path="/home" component ={Home}/>
-                <Route path="/login" component ={Login}/>
-                <Route path="/usuario" component ={Usuario}/>
+                <Route path="/cadastro" element ={<Cadastro />} />
+                <Route path="/home" element ={<Home />}/>
+                <Route path="/login" element ={<Login />}/>
+                <Route path="/usuario" element ={<Usuario />}/>
             </Routes>
             </BrowserRouter>
         )
