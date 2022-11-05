d506a1e16b47b9c681929aaf372c71463e7a9389
gabrovnikov <gabrvalete@gmail.com>
2022-11-05 18:04:16 -0300

    CSS Home com cores e pré Footer

--- projetocpegp1/src/Pages/Home/Home.js
+++ projetocpegp1/src/Pages/Home/Home.js
@@ -17,13 +17,15 @@ function Home(){
 
 
         <div className="containerQuadradoeCrash">
-            <div className="quadradoCinza"></div>
+            <div className="quadradoCinza">
+            " Jogue centenas de jogos de alta qualidade diretamente pelo seu PC ou via nuvem. Atualizações constantes, você sempre terá o que jogar!!";
+            </div>
             <div className="crash4"></div>
         </div>
        
        <div className="container2RetanguloeJogos">
 
-             <div className="retanguloCinza1"></div>
+             <div className="retanguloCinza1">"Os maiores sucessos estão aqui!!"</div>
 
         <div className="containerRetangulosJogosHome"> 
                 <div className="retanguloJogo1"></div>
@@ -52,17 +54,137 @@ function Home(){
 
         <div className="containerEscolhaDePlanos">
 
-            <div className="quadradoBrancoPlanos1"></div>
-            <div className="quadradoBrancoPlanos2"></div>
-            <div className="quadradoBrancoPlanos3"></div>
-            <div className="quadradoBrancoPlanos4"></div>
+        <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Iceland"></link>
+          
+        
+          <div className="container_planos">
+            <div className="SelecioneOPlano">
+                <h1 className="selecione"> Selecione o plano de interesse</h1>
+            </div>
+           
+              <div className="planos">
+              
+                
+                <div className="planoStarter">
+                  <h2>Starter</h2>
+                    <p>
+                    - Acesso a 100 jogos
+                    <br/>
+                    - Qualidade Full-HD
+                    <br/>
+                    - Catálogo atualizado constantemente
+                    <br/>
+                    </p>
+                    <br/> <br/> <br/> <br/> <br/>
+                    <br/> <br/> <br/> <br/> <br/>
+                    <br/> <br/> <br/> <br/>
+                    <h4>
+                    <a href="/cadastro">R$ 14,90</a>
+                    </h4>
+                  
+                </div>
+                
+
+                <div className="planoMedium">
+                <h2>Medium</h2>
+                  <p>
+                    - Acesso a 100 jogos
+                    <br/>
+                    - Qualidade Full-HD
+                    <br/>
+                    - Catálogo atualizado constantemente
+                    <br/>
+                    - Retrocompatibilidade
+                    <br/>
+                    - Descontos exclusivos
+                    <br/>
+                  </p>
+                  <br/> <br/> <br/> <br/> <br/>
+                  <br/> <br/> <br/> <br/> <br/>
+                  <br/> <br/>
+                  
+                  <h4>
+                  <a href="/cadastro">R$ 24,90</a>
+                    </h4>
+               
+                </div>
+                
+
+                <div className="planoPro">
+                <h2>Pro</h2>
+                <p>
+                    - Biblioteca completa
+                    <br/>
+                    - Qualidade Full-HD
+                    <br/>
+                    - Catálogo atualizado constantemente
+                    <br/>
+                    - Retrocompatibilidade
+                    <br/>
+                    - Descontos exclusivos
+                    <br/>
+                    - Use em vários dispositivos simultaneamente
+                    <br/>
+                    - Lançamentos disponíveis no primeiro dia
+                    <br/> 
+                    - Acesso a serviços parceiros
+                    <br/> <br/> <br/> <br/> <br/> 
+                    <h4>
+                    <a href="/cadastro">R$ 32,90</a>
+                    </h4>     
+                </p>
+               
+                </div>
+
+
+                <div className="planoUltimate">
+                  <h2>Ultimate</h2>
+                  <p>
+                      - Biblioteca completa
+                      <br/>
+                      - Qualidade 4K
+                      <br/>
+                      - Catálogo atualizado constantemente
+                      <br/>
+                      - Retrocompatibilidade
+                      <br/>
+                      - Descontos exclusivos
+                      <br/>
+                      - Use em vários dispositivos simultaneamente
+                      <br/>
+                      - Jogos na nuvem
+                      <br/>
+                      - Lançamentos disponíveis no primeiro dia
+                      <br/> 
+                      - Acesso a serviços parceiros
+                      <br/>
+                      - Acesso vitalício a 2 jogos todo mês     
+                  </p>
+                  <br/>
+                      <h4>
+                      <a href="/cadastro">R$ 39,90</a>
+                      </h4>
+                
+                </div>
+
+              </div>  
+          </div>
+        
 
         </div>
         
 
         <div class="quadradoFeedbacks">
+        <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Iceland"></link>
+            <div className="tituloDoFeedback">
+                <h1 className="vejaFeedback">Veja o feedback de nossos usuários</h1>
+            </div>
+        
         <div className="containerdosFeedbacks">
 
+        
             
             <div class="feedback1"></div>
 
@@ -79,6 +201,11 @@ function Home(){
       <div className="retânguloCinzaAbaixo"></div>
 
       <div className="espacoAbaixoNulo"></div>
+
+      
+     
+    
+      
 
       <div className="footer"></div>
       <a href="/home" > <Logo /> </a>
@@ -110,3 +237,6 @@ function Logo(){
 
 
 export default Home;
+
+
+
