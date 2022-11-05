2b582e5e674130bd1a5b2dccfd6cfb2c4082e8d9
Leandro Lopo <leandro.loopo@gmail.com>
2022-11-05 14:18:33 -0300

    css do escolha de plano

--- projetocpegp1/src/Pages/EscolhaPlano/EscolhaPlano.js
+++ projetocpegp1/src/Pages/EscolhaPlano/EscolhaPlano.js
@@ -1,12 +1,14 @@
 import React from "react"
-import "./escolhaplano.css";
+import "./EscolhaPlano.css";
 
 function EscolhaPlano(){
     return(
       <div className="background_planos">
+        <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Iceland"></link>
         <div className="base">
           <div className="container_planos">
-            <h1> Selecione o plano de interesse</h1>
+            <h1 className="selecione"> Selecione o plano de interesse</h1>
               <div className="planos">
                 
                 <div className="plano">
@@ -19,6 +21,16 @@ function EscolhaPlano(){
                     - Catálogo atualizado constantemente
                     <br/>
                     </p>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <h4>
+                    R$ 14,90
+                    </h4>
                   <input  id = "STR" type = "radio" name = "plano"/>
                 </div>
                 
@@ -37,6 +49,14 @@ function EscolhaPlano(){
                     - Descontos exclusivos
                     <br/>
                   </p>
+                  <br/>
+                  <br/>
+                  <br/>
+                  <br/>
+                  <br/>
+                  <h4>
+                    R$ 24,90
+                    </h4>
                 <input  id = "MID" type = "radio" name = "plano"/>
                 </div>
                 
@@ -59,36 +79,48 @@ function EscolhaPlano(){
                     - Lançamentos disponíveis no primeiro dia
                     <br/> 
                     - Acesso a serviços parceiros
-                    <br/>     
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <h4>
+                    R$ 32,90
+                    </h4>     
                 </p>
                 <input  id = "PRO" type = "radio" name = "plano"/>
                 </div>
 
 
                 <div className="plano">
-                <h2>Ultimate</h2>
-                <p>
-                    - Biblioteca completa
-                    <br/>
-                    - Qualidade 4K
-                    <br/>
-                    - Catálogo atualizado constantemente
-                    <br/>
-                    - Retrocompatibilidade
-                    <br/>
-                    - Descontos exclusivos
-                    <br/>
-                    - Use em vários dispositivos simultaneamente
-                    <br/>
-                    - Jogos na nuvem
-                    <br/>
-                    - Lançamentos disponíveis no primeiro dia
-                    <br/> 
-                    - Acesso a serviços parceiros
-                    <br/>
-                    - Acesso vitalício a 2 jogos todo mês     
-                </p>
-                <input  id = "UTM" type = "radio" name = "plano"/>
+                  <h2>Ultimate</h2>
+                  <p>
+                      - Biblioteca completa
+                      <br/>
+                      - Qualidade 4K
+                      <br/>
+                      - Catálogo atualizado constantemente
+                      <br/>
+                      - Retrocompatibilidade
+                      <br/>
+                      - Descontos exclusivos
+                      <br/>
+                      - Use em vários dispositivos simultaneamente
+                      <br/>
+                      - Jogos na nuvem
+                      <br/>
+                      - Lançamentos disponíveis no primeiro dia
+                      <br/> 
+                      - Acesso a serviços parceiros
+                      <br/>
+                      - Acesso vitalício a 2 jogos todo mês     
+                  </p>
+                  <br/>
+                      <h4>
+                      R$ 39,90
+                      </h4>
+                <input  id = "UTM" type = "radio" name = "plano">
+                  
+                </input>
                 </div>
 
               </div>  
