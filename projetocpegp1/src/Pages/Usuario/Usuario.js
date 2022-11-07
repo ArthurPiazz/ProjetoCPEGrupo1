e2a02774398d5ae28cc58dc04f6f1af91d12b9dc
gabrovnikov <gabrvalete@gmail.com>
2022-11-07 19:12:50 -0300

    Usuario completo incluso

--- projetocpegp1/src/Pages/Usuario/Usuario.js
+++ projetocpegp1/src/Pages/Usuario/Usuario.js
@@ -1,15 +1,99 @@
 import React from 'react';
-import "./usuario.css";
+import "./CSS/Usuario.css"
 
 function Usuario() {
-
+  
+  //const [nome, setNome] =  useState();
+  //
   return (
-    <div className="background_usuario">
-    <h1>USUARIO</h1>
-    <br/>
-    <button>EDITAR PERFIL</button>
+<div>
+<div class ="Background-Usuario">
+    <head>
+        <link rel="stylesheet" href="CSS/Usuario.css"></link>
+        
+    </head>    
+<div class="body-Usuario">
+    <Logo />
+    <div class="RetangulodoNome-Usuario">
+      <br></br><br></br>
+      <div class="ImagemPerfil-Usuario" />
+      <div class="NomedoUsuario-Usuario"><NomeDoUsuario /></div>
+    
     </div>
+    <div class="body-Informações-Usuario">
+      <div class="Texto-Jogos-Usuario">Jogos Recentes:</div>
+      <GetJogosRecentes />
+      <div>
+        <img class="Primeiro-Jogo-Usuario" src= "CSS\newproject.png" alt="" />
+        <div class = "Primeiro-Texto-Usuario"><br /><GetHoras /></div>
+
+        <img class="Segundo-Jogo-Usuario" src= "CSS\newproject.png" alt="" />
+        <div class = "Segundo-Texto-Usuario"><br /><GetHoras /></div>
+
+        <img class="Terceiro-Jogo-Usuario" src= "CSS\newproject.png" alt="" />
+        <div class = "Terceiro-Texto-Usuario"><br /><GetHoras /></div>
+    </div>
+    </div>
+    <div class="body-Amigos">
+    <div class="Texto-Jogos-Usuario">Amigos Online</div>
+    <div class= "Quadrado-amigos">
+    <div class="ImagemPerfilAmigo" />
+    <div class="TextoPerfilAmigo"><NomeDoUsuario/></div>
+    </div>
+    <div class= "Quadrado-amigos">
+    <div class="ImagemPerfilAmigo" />
+    <div class="TextoPerfilAmigo"><NomeDoUsuario/></div>
+    </div>
+    <div class= "Quadrado-amigos2">
+    <div class="ImagemPerfilAmigo" />
+    <div class="TextoPerfilAmigo"><NomeDoUsuario/></div>
+    </div>
+    <div class= "Quadrado-amigos2">
+    <div class="ImagemPerfilAmigo" />
+    <div class="TextoPerfilAmigo"><NomeDoUsuario/></div>
+    </div>
+    <div class= "Quadrado-amigos3">
+    <div class="ImagemPerfilAmigo" />
+    <div class="TextoPerfilAmigo"><NomeDoUsuario/></div>
+    </div>
+    <div class= "Quadrado-amigos3">
+    <div class="ImagemPerfilAmigo" />
+    <div class="TextoPerfilAmigo"><NomeDoUsuario/></div>
+    </div>
+    </div>
+    <a class="button-1-Usuario" href="http://localhost:3000/Home">Retorna para Home</a>
+    <div class="Rodape-Usuario"><Logo /><br /><br /><br />
+    
+    </div> 
+</div>
+</div>
+
+</div>
   );
 }
+
+function Logo(){
+    return(
+    <div>
+    <div class="logo"></div>
+    <div class="logo-parte-roxa"></div>
+    <div class="logo-parte-branca"></div>
+    <div class="logo-texto">GAMECASTING</div>
+    </div>
+    );
+}
+
+function NomeDoUsuario(){ //implementação de backend nao feita
+  return("Não Encontrado");
+}
+
+function GetJogosRecentes(){
+  return("")
+}
+
+function GetHoras(){
+  return("0" + " Horas Jogadas")
+}
+
 
 export default Usuario;
